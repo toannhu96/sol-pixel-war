790ae95bdc00ca78fa0442cdd6acc8725c85b645
Toan Nhu <toanbk21096@gmail.com>
2024-07-30 11:24:35 +0700

    mod: update font size

--- src/server.js
+++ src/server.js
@@ -56,7 +56,8 @@ app.get("/api/get-image", async (req, res) => {
 
     // Draw grid and ordinal numbers
     ctx.strokeStyle = "#ccc";
-    ctx.font = "10px Arial";
+    // ctx.font = "10px Arial";
+    ctx.fontSize = 10;
     ctx.fillStyle = "#000";
 
     for (let i = 0; i <= gridSize; i++) {
